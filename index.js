f60a2e41f91b92dc4fd8ebe826e13c6603c445dd
Barsha Dahal <dahalbarsha19@gmail.com>
2020-07-21 23:55:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  for(let i=0;i<dwarves.length;i++){
+   dwarves.push(`${i+1}.${dwarves[i]}`)
+  }
+  return dwarves;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i=0;i<planeteerCalls.length;i++){
+    `${planeteerCalls[i].toUpperCase()}`
+  }
+  return `${planeteerCalls.join("!")}`
 }
 
 function longPlaneteerCalls(words) {
+  for (let i=0; i<words.length; i++){
+    if(words[i].length<4){
+      return true;
+    }
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+  for(let i=0;i<foods.length;i++){
+    if (foods[i]===gouda){
+      return `gouda`
+    }else if(foods[i]===cheddar){
+      return `cheddar`
+    }else{
+      return `no cheese!`
+    }
+  }
 }
